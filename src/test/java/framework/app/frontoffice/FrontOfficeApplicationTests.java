5905b7c84f6af1a743706439ffc8f15c4c8164b0
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2026-01-27 15:01:59 +0300

    init

---
+++ src/test/java/framework/app/frontoffice/FrontOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package framework.app.frontoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
