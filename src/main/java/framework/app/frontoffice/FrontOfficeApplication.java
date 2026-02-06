4e15a01718e040cdefa7ee721bf19df6db0b7c68
eXen54 <168262555+eXen54@users.noreply.github.com>
2026-02-06 16:14:22 +0300

    mise en place des appels d'api
    (cherry picked from commit 12e88acbf6e5b224dc49f9271c80699d2c316cfd)

--- src/main/java/framework/app/frontoffice/FrontOfficeApplication.java
+++ src/main/java/framework/app/frontoffice/FrontOfficeApplication.java
@@ -2,12 +2,19 @@ package framework.app.frontoffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FrontOfficeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FrontOfficeApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
