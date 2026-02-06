6b3cf38f97f9258ecd897b5855e01f71a4dec8dc
eXen54 <168262555+eXen54@users.noreply.github.com>
2026-02-06 14:50:50 +0300

    création de reservationDTO

---
+++ src/main/java/framework/app/frontoffice/dto/ReservationDTO.java
@@ -0,0 +1,52 @@
+package framework.app.frontoffice.dto;
+
+import java.sql.Timestamp;
+
+public class ReservationDTO {
+    private Integer nb_passager;
+    private String id_client;
+    private String nom_hotel;
+    private Timestamp date_reservation;
+
+    public ReservationDTO() {
+    }
+
+    public ReservationDTO(Integer nb_passager, String id_client, String nom_hotel, Timestamp date_reservation) {
+        this.nb_passager = nb_passager;
+        this.id_client = id_client;
+        this.nom_hotel = nom_hotel;
+        this.date_reservation = date_reservation;
+    }
+
+    public Integer getNb_passager() {
+        return nb_passager;
+    }
+
+    public void setNb_passager(Integer nb_passager) {
+        this.nb_passager = nb_passager;
+    }
+
+    public String getId_client() {
+        return id_client;
+    }
+
+    public void setId_client(String id_client) {
+        this.id_client = id_client;
+    }
+
+    public String getNom_hotel() {
+        return nom_hotel;
+    }
+
+    public void setNom_hotel(String nom_hotel) {
+        this.nom_hotel = nom_hotel;
+    }
+
+    public Timestamp getDate_reservation() {
+        return date_reservation;
+    }
+
+    public void setDate_reservation(Timestamp date_reservation) {
+        this.date_reservation = date_reservation;
+    }
+}
